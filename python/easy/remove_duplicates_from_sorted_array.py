12f75d2f9d7d9350bfdcbc2a02d5ca3f42db781e
Moveton <creating.smth@gmail.com>
2024-09-14 17:19:40 +0200

    Python: remove duplicates from sorted array

---
+++ python/easy/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def remove_duplicates_from_sorted_array(nums: List[int]) -> int:
+    if not nums:
+        return 0
+
+    k = 1
+
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            nums[k] = nums[i]
+            k += 1
+
+    return k
